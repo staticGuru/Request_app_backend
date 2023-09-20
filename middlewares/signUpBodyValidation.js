166a53c9b452ae7244498706660dfec9751cc1a2
Guruvignesh M <vigneshguru274@gmail.com>
2023-09-20 13:34:38 +0530

    add the signup body validations

--- middlewares/signUpBodyValidation.js
+++ middlewares/signUpBodyValidation.js
@@ -1,31 +1,28 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
-const signUpBodyValidation = async(req,res,next) => {
-  const body = req.body
-  try{
+const signUpBodyValidation = async (req, res, next) => {
+  const body = req.body;
+  try {
     const result = Joi.object({
-    password: Joi
-      .string()
-      .min(8)
-      .max(20).required(),
-    role: Joi.string().required().valid("Admin","User"),
-    email: Joi.string().required().email(),
-    userName:Joi.string().required(),
-      // .regex(/^[\w!.%+\-]+@[\w\-]+(?:\.[\w\-]+)+$/)
-    confirmPassword: Joi.string().required(),
-    companyName: Joi.string().required().min(3).max(30),
-    isAgree: Joi.boolean().required().valid(true),
-    //userName: Joi.string().required().min(4).max(20)
-    })
-    const validation = result.validate(body)
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      middleName: Joi.string(),
+      email: Joi.string().required().email(),
+      password: Joi.string().min(8).max(20).required(),
+      reEnterPassword: Joi.string().required(),
+      orgId: Joi.number().required(),
+    });
+    const validation = result.validate(body);
 
-    if(validation.error){
-      res.status(401).json({status: "fail", message: validation.error.details})
-    }else{
-      next()
+    if (validation.error) {
+      res
+        .status(401)
+        .json({ status: "fail", message: validation.error.details });
+    } else {
+      next();
     }
-  }catch(err){
-    res.status(500).json({status: "fail", message: err.message})
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: err.message });
   }
-}
-module.exports = signUpBodyValidation
+};
+module.exports = signUpBodyValidation;
